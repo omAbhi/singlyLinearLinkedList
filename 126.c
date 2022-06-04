5278547e1568689bafa81dd76c08d1451119bfd1
omAbhi <106860931+omAbhi@users.noreply.github.com>
2022-06-04 12:30:07 +0530

    Add files via upload

---
+++ 126.c
@@ -0,0 +1,97 @@
+# include<stdio.h>
+# include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;   //*
+};
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+void Display(PNODE head)
+{
+	printf("data from L.L is:\n");
+	while(head!=NULL)
+	{
+		printf("|%d|->",head->data);
+		head=head->next;
+	}
+	printf("\n");
+}
+
+int count(PNODE head)
+{
+	return 0;
+}
+
+void InsertFirst(PPNODE head, int no)
+{
+	PNODE newn= NULL;
+	newn= (PNODE)malloc(sizeof(NODE));
+	newn->data= no;
+	newn->next= NULL;
+	newn->prev= NULL;   //*
+	if(*head==NULL)
+	{
+		*head=newn;
+	}
+	else
+	{
+		newn->next= *head;
+		(*head)->prev= newn;    //*
+		*head= newn;
+	}
+}
+void InsertLast(PPNODE head,int no)
+{
+	PNODE temp=NULL;
+		PNODE newn= NULL;
+	newn= (PNODE)malloc(sizeof(NODE));
+	newn->data= no;
+	newn->next= NULL;
+	newn->prev= NULL;   //*
+	if(*head==NULL)
+	{
+		*head=newn;
+	}
+	else
+	{
+		temp= *head;
+		while(temp->next!=NULL)
+		{
+			temp= temp;
+		}
+		temp->next=newn;
+		newn->prev=temp;   //*   
+	}
+}
+
+void DeleteFirst(PPNODE head)
+{
+	if(*head==NULL)
+}
+
+void DeleteLast(PPNODE head)
+{}
+
+void InsertAtPosition(PPNODE head, int no,int pos)
+{}
+
+void DeleteAtPos(PPNODE head,int pos)
+{}
+int main()
+{
+	int ret=0;
+	PNODE first=NULL;
+	InsertFirst(&first,21);
+	InsertFirst(&first,11);
+	
+	InsertLast(&first,51);
+	InsertLast(&first,101);
+	
+	Display(first);
+	ret= Count(first);
+	printf("number of nodes are: %d",ret);
+	return 0;
+}
